96785ac49d8e70b7c5323abbcd54d3a7c6e104a3
Nikita <mitichkin0303@mail.ru>
2023-12-11 12:35:34 +0200

    Пытаюсь кодить в  IJ

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,76 @@
-import java.time.LocalTime;
 import java.util.Scanner;
 
+/*
+ Создайте класс "Пользователь" с приватными полями имя, возраст и электронная почта.
+Реализуйте методы getName(), getAge() и getEmail(), которые возвращают соответствующие значения.
+Добавьте метод sendEmail(), который отправляет электронное письмо пользователю, и метод changePassword(),
+который позволяет пользователю изменить пароль.
+*/
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Как вас зовут? ");
-        String name = scanner.nextLine();
-        LocalTime time = LocalTime.now();
-        int hour = time.getHour();
+        User user1 = new User();
+        user1.registration();
+        user1.showUserData();
+    }
 
-        if (hour >= 5 && hour < 12) {
-            System.out.println("Доброе утро, " + name + "!");
-        } else if (hour >= 12 && hour < 18) {
-            System.out.println("Доброе день, " + name + "!");
-        } else if (hour >= 18 && hour < 23) {
-            System.out.println("Доброе вечер, " + name + "!");
-        } else {
-            System.out.println("Доброе ночи, " + name + "!");
+}
 
-        }
+class User{
+    Scanner scanner = new Scanner(System.in);
+
+    private String name;
+    private  String email;
+    private int age;
+
+    public void registration(){
+        System.out.println("Здравствуйте, необходимо зарегистрироваться.");
+        System.out.print("Введите ваше имя: ");
+        name = scanner.nextLine();
+        System.out.print("Введите ваш email: ");
+        email = scanner.nextLine();
+        System.out.print("Введите ваш возраст: ");
+        age = scanner.nextInt();
+
+        System.out.println("Здравствуйте " +name);
+
+
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sendEmail(){
+        System.out.println("Напишите почту на которую хотите отправить письмо: ");
+        String nameSendEmail = scanner.nextLine();
+        System.out.println("Напишите письмо: ");
+        String userMail = scanner.nextLine();
+
+        System.out.println("Ваше письмо " + userMail +" отправлено на адрес "+ nameSendEmail);
+    }
+
+    public void showUserData(){
+        System.out.println("Ваши данные: email = "+email+", возраст = "+age+", имя = "+name+".");
+
     }
 }
 
